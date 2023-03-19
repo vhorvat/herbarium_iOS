9bfce9f1232992bec6ee2703ed4dcc043265e3c7
Viktor Horvat <vh51195@fer.hr>
2023-03-19 22:42:13 +0100

    1903
    home screen re-do
    started working on data screen

--- watchpstoryboard/AllDataViewController.swift
+++ watchpstoryboard/AllDataViewController.swift
@@ -7,10 +7,33 @@
 
 import UIKit
 
-class AllDataViewController: UIViewController {
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
 
+class AllDataViewController: UIViewController {
+    
+    @IBOutlet weak var current_time: UILabel!
+    @IBOutlet weak var current_date: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        let timeString = String(hour) + ":" + String(minutes)
+        current_time.text = timeString
+        
+        let formatDate = date.getFormattedDate(format: "EEEE, MMM d, yyyy.")
+        current_date.text = formatDate
+
+        
         if ((Shared.instance.peripheral?.name) != nil){
             print(Shared.instance.peripheral.name)
         } else {
